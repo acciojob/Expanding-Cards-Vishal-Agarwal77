647b97a0d28341cd09b3f83f67d8f660b959a67b
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 00:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const img=document.getElementsByClassName("panel");
+console.log(img);
+let active_ele=document.getElementsByClassName("active")[0];
+for(let i=0;i<img.length;i++){
+	let ele=img[i];
+	ele.addEventListener("click",()=>{
+		active_ele.classList.remove("active");
+		ele.classList.add("active");
+		active_ele=ele;
+	})
+}
